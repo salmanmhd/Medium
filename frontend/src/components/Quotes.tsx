89525774b89978fa68ac764d1b8d8539cb21166a
Md Salman <mhdsalman010@gmail.com>
2025-03-02 12:55:16 +0530

    initialized fe, setup tailwind

---
+++ frontend/src/components/Quotes.tsx
@@ -0,0 +1,9 @@
+function Quotes() {
+  return (
+    <div className="flex h-screen items-center justify-center bg-slate-200">
+      <div className="bg-red-300">Quotes</div>
+    </div>
+  );
+}
+
+export default Quotes;
---
+++ frontend/src/pages/Signup.tsx
@@ -0,0 +1,12 @@
+import Quotes from "../components/Quotes";
+
+function Signup() {
+  return (
+    <div className="grid grid-cols-2">
+      <div>sign up</div>
+      <Quotes />
+    </div>
+  );
+}
+
+export default Signup;
